b8b5c9861faa14160f6591e10f9509347ce79176
dipanjan.haldar <dipanjan.haldar@everbridge.com>
2020-01-14 19:08:37 -0500

    adding code

---
+++ front-end/myfirstreactapp/src/App.js
@@ -0,0 +1,19 @@
+import React , {Component} from 'react'
+import './App.css'
+
+
+class myReactApp extends Component{
+
+  render() {
+
+    return(
+        <div>
+          Hello Dipanjan
+        </div>
+
+    );
+  }
+}
+
+export default myReactApp
+
